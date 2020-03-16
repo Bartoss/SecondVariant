91eb90d33f824d744984aee8deeb85538dd6191d
Bartoss <sarata.bartlomiej@student.pk.edu.pl>
2020-03-16 18:14:31 +0100

    Initial Commit

---
+++ src/main/java/com/bartolomeo/SecondVariant/SecondVariantApplication.java
@@ -0,0 +1,13 @@
+package com.bartolomeo.SecondVariant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondVariantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondVariantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartolomeo/SecondVariant/SecondVariantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartolomeo.SecondVariant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondVariantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
